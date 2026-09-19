822e75486e96ac33b89fd12abf50d4b545f5be20
max <max.koeg@gmail.com>
2026-09-19 13:43:03 -0700

    touchpoint 3

--- main.cpp
+++ main.cpp
@@ -27,9 +27,22 @@ int main() {
         cin >> employees[i].name;
         cout << "Total sales count: ";
         cin >> employees[i].numSales;
+        employees[i].sales = new double[employees[i].numSales];
         for(int j = 0; i < employees[i].numSales; j++) {
-            
+            cout << "Contract value of sale #" << j << ": ";
+            cin >> employees[i].sales[j];
         }
 
     }
+
+    cout << "<<<Sales Database>>>";
+    for(int i = 0; i < size; i++) {
+        cout << "Employee #" << i << endl;
+        cout << "    -Name: " << employees[i].name << endl;
+        cout << "    -Total Sales: " << employees[i].numSales << endl;
+        cout << "    -Sales History: " << endl;
+        for(int j = 0; i < employees[i].numSales; j++) {
+            cout << "        -";
+            cout << employees[i].sales[j];
+        }
 }
