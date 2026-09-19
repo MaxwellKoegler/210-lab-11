259c0740fc17cb107ceff62b392216ff60425832
max <max.koeg@gmail.com>
2026-09-19 13:31:48 -0700

    touchpoint 2

--- main.cpp
+++ main.cpp
@@ -2,18 +2,34 @@
 
 #include <string>
 #include <iostream>
-#include <array>
 
 using namespace std;
 
 //this struct stores recent deals and quota information of employees
 struct Employee {
     string name;
-    array<string, 5> recentDeals; 
-    double percentQuotaMet;
+    int numSales;
+    double* sales;
 };
 
 
 int main() {
-    
+    int size;
+    cout << "How many sales employees would you like in your database? ";
+    cin >> size;
+
+    Employee* employees = new Employee[size];
+
+    for(int i = 0; i < size; i++) {
+        cout << "Information for employee # " << size << endl;
+        cout << "------------------------------" << endl;
+        cout << "Name: ";
+        cin >> employees[i].name;
+        cout << "Total sales count: ";
+        cin >> employees[i].numSales;
+        for(int j = 0; i < employees[i].numSales; j++) {
+            
+        }
+
+    }
 }
