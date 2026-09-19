ab52af23d8e5a482979755a6d1c3ab5560ca80b8
max <max.koeg@gmail.com>
2026-09-19 13:19:46 -0700

    touchpoint 1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,19 @@
 //Maxwell Koegler | COMSC 210 | lab 11
 
+#include <string>
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+//this struct stores recent deals and quota information of employees
+struct Employee {
+    string name;
+    array<string, 5> recentDeals; 
+    double percentQuotaMet;
+};
+
+
+int main() {
+    
+}
